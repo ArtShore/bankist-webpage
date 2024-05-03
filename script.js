a0df94f86db49b02b5709c2e4b98f562f53df139
artshore <90263675+ArtShore@users.noreply.github.com>
2024-05-03 20:41:09 +0330

    replaced scroll event for sticky nav with intersection observer

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const section1 = document.querySelector('#section--1');
 const tabBtnParent = document.querySelector('.operations');
 const tabBtns = document.querySelectorAll('.operations__tab');
 const tabComps = document.querySelectorAll('.operations__content');
+const navbar = document.querySelector('.nav');
 
 // Modal Window //
 const openModal = function (e) {
@@ -75,7 +76,6 @@ tabBtnParent.addEventListener('click', function (e) {
     .classList.add('operations__content--active');
 });
 // Menu fade animation //
-const navbar = document.querySelector('.nav');
 const fadeNav = function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
@@ -90,13 +90,41 @@ const fadeNav = function (e) {
 navbar.addEventListener('mouseover', fadeNav.bind(0.5));
 navbar.addEventListener('mouseout', fadeNav.bind(1));
 // Sticky navigation //
-const sectOneCoords = section1.getBoundingClientRect();
-console.log(sectOneCoords);
-window.addEventListener('scroll', function () {
-  // console.log(window.scrollY);
-  if (window.scrollY > sectOneCoords.top) {
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');
-  }
-});
+// const sectOneCoords = section1.getBoundingClientRect();
+// console.log(sectOneCoords);
+// window.addEventListener('scroll', function () {
+//   // console.log(window.scrollY);
+//   if (window.scrollY > sectOneCoords.top) {
+//     navbar.classList.add('sticky');
+//   } else {
+//     navbar.classList.remove('sticky');
+//   }
+// });
+// Sticky navigation bar via intersection obs api //
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+// const obsOpts = {
+//   root: null,
+//   threshold: 0.1,
+// };
+// const observer = new IntersectionObserver(obsCallback, obsOpts);
+// observer.observe(section1);
+const header = document.querySelector('.header');
+const navbarHeight = navbar.getBoundingClientRect().height;
+console.log(navbarHeight);
+const navCallback = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) navbar.classList.add('sticky');
+  else navbar.classList.remove('sticky');
+};
+const navOpts = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navbarHeight}px`,
+};
+// console.log(header);
+const headerObs = new IntersectionObserver(navCallback, navOpts);
+headerObs.observe(header);
